f75cab9a5abece6992ed85c1673dd16d8e3de74f
Kingshuk <saha.kingshuk@yahoo.com>
2019-08-31 15:18:07 +0530

    booking added

--- app/src/main/java/com/thetripod/synq/MainActivity.java
+++ app/src/main/java/com/thetripod/synq/MainActivity.java
@@ -5,17 +5,15 @@ import android.os.Bundle;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.firebase.ui.database.SnapshotParser;
-import com.google.android.gms.common.internal.Constants;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+
 import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
+
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
