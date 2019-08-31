fa4ba465117b2cbee163d03f6da330fd5f95c0cd
Kingshuk <saha.kingshuk@yahoo.com>
2019-08-31 18:20:08 +0530

    login done

---
+++ app/src/main/java/com/thetripod/synq/LoginActivity.java
@@ -0,0 +1,67 @@
+package com.thetripod.synq;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends FragmentActivity  implements NumberFragment.OnFragmentInteractionListener,OTPFragment.OnFragmentInteractionListener{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // the fragment_container FrameLayout
+        if (findViewById(R.id.container_login) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            NumberFragment firstFragment = new NumberFragment();
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            firstFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container_login, firstFragment).commit();
+
+        }
+
+
+
+
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(FirebaseAuth.getInstance().getCurrentUser()!=null)
+        {
+            Intent intent=new Intent(this,MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+    }
+}
---
+++ app/src/main/java/com/thetripod/synq/OTPFragment.java
@@ -0,0 +1,181 @@
+package com.thetripod.synq;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskExecutors;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+
+
+public class OTPFragment extends Fragment {
+
+    private String phone_number;
+    private EditText OTP;
+    private FirebaseAuth mAuth;
+    private OnFragmentInteractionListener mListener;
+    private String verificationCode;
+    private String otp_final;
+
+    public OTPFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static OTPFragment newInstance(String param1, String param2) {
+        OTPFragment fragment = new OTPFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            phone_number = getArguments().getString("NUMBER_TRANSFER");
+            Toast.makeText(getContext(),phone_number, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_otp, container, false);
+        Button confirm_otp = rootView.findViewById(R.id.button_confirm_otp);
+        OTP=rootView.findViewById(R.id.enter_otp);
+
+        mAuth = FirebaseAuth.getInstance();
+        phoneVerificationCode(phone_number);
+        confirm_otp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                otp_final=OTP.getText().toString().trim();
+                if(otp_final.isEmpty()||otp_final.length()<6)
+                {
+                    Toast.makeText(getContext(),"Wrong OTP", Toast.LENGTH_LONG).show();
+
+                }
+                else
+                verifyCode(otp_final);
+            }
+        });
+        return rootView;
+    }
+
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+
+
+
+    private void verifyCode(String code)
+    {
+        PhoneAuthCredential credential= PhoneAuthProvider.getCredential(verificationCode,code);
+        signInWithCredential(credential);
+
+    }
+
+    private void signInWithCredential(PhoneAuthCredential credential) {
+
+        mAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+
+                    Intent intent=new Intent(getContext(),MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }else
+                {
+                    Toast.makeText(getContext(),task.getException().getMessage(),Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+    }
+
+
+    private void phoneVerificationCode(String number)
+    {
+        // progressBar.setVisibility(View.VISIBLE);
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(number,60, TimeUnit.SECONDS, TaskExecutors.MAIN_THREAD,mCallback);
+    }
+
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallback= new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        @Override
+        public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+            verificationCode=s;
+        }
+        // esheche bol 713017
+
+        @Override
+        public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+
+            String code=phoneAuthCredential.getSmsCode();
+            if(code!=null)
+            {
+
+                OTP.setText(code);
+                verifyCode(code);
+            }
+        }
+
+
+        @Override
+        public void onVerificationFailed(@NonNull FirebaseException e) {
+
+            Toast.makeText(getContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+        }
+    };
+
+
+
+}
+
